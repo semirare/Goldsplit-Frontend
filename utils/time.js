24907105aaace304d6d4c8af7c895a804a62a6e3
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2023-08-18 18:27:00 -0700

    round ms to 2 places

--- utils/time.js
+++ utils/time.js
@@ -10,10 +10,27 @@ export const timeToSeconds = (time) => {
 };
 
 export const msToTime = (ms) => {
-    var milliseconds = parseInt((ms % 1000)),
+    var milliseconds = parseInt(ms % 1000),
         seconds = parseInt((ms / 1000) % 60),
         minutes = parseInt((ms / (1000 * 60)) % 60),
         hours = parseInt((ms / (1000 * 60 * 60)) % 24);
+
+    // if milliseconds in < 10 pad with a trailing 0
+    milliseconds = (milliseconds < 10) ? milliseconds + "0" : milliseconds
+    if (milliseconds >= 100) {
+        //if milliseconds is > 100 round it to the nearest 10
+        milliseconds = Math.round(milliseconds / 10) * 10;
+        if (milliseconds === 1000) {
+            //if rounding to the nearest 10 brought us to 1000, add a second
+            //and set milliseconds to 00
+            seconds += 1;
+            milliseconds = "00";
+        }
+        else {
+            //if rounding did not bring us to 1000, keep just the first 2 chars
+            milliseconds = milliseconds.toString().slice(0, 2);
+        }
+    }
 
     if (hours > 0) {
         //pad minutes and seconds with leading 0 if less than 10
